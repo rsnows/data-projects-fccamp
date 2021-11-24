f310839cc20467e79802856bf698803c752ccd80
Arnaldo Silva <rsnowsdev@gmail.com>
2021-11-24 19:10:37 -0300

    Add stock price and dna count projects

---
+++ 1-simple-stock-price/myapp.py
@@ -0,0 +1,27 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write("""
+# Simple Stock Price App
+
+Shown are the stock **closing price** and **volume** of Google!
+""")
+
+tickerSymbol = 'GOOGL'
+
+tickerData = yf.Ticker(tickerSymbol)
+
+tickerDf = tickerData.history(period='20d', start='2010-5-31', end='2021-5-31')
+
+st.write("""
+## Closing Price
+""")
+
+st.line_chart(tickerDf.Close)
+
+st.write("""
+## Volume Price
+""")
+
+st.line_chart(tickerDf.Volume)
